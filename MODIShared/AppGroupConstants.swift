fc812c824a8bbb5a37bc76a30320845a84f73b8f
20kyi <20kyi@naver.com>
2026-07-09 17:27:57 +0900

    chore: Bundle ID와 App Group을 modiapp 식별자로 변경
    Apple Developer 등록 충돌을 피하기 위해 앱·위젯 번들 ID와 App Group을 com.storybuild.modiapp으로 통일한다.

--- MODIShared/AppGroupConstants.swift
+++ MODIShared/AppGroupConstants.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 enum AppGroupConstants {
-    static let identifier = "group.com.storybuild.MODI"
+    static let identifier = "group.com.storybuild.modiapp"
     static let widgetKind = "MODIWidget"
     static let urlScheme = "modi"
 }
